86c7105c9ae9d2a385f1a0f7d06dd50982aaec7a
Marcos Vinício <marcosviniciohd@hotmail.com>
2019-01-22 16:40:37 -0200

    Teste

---
+++ src/Uri1004.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Uri1004 {
+    public static void main(String[] args) {
+       int a, b, PROD;
+
+        Scanner in = new Scanner(System.in);
+
+        a = in.nextInt();
+        b = in.nextInt();
+
+        PROD = a * b;
+
+        System.out.println("PROD = " + PROD);
+
+    }
+}
